dd828141bffb14c182d8b07c0648fd63ebe85472
GooksanGom <stolen6394@gmail.com>
2022-05-18 18:52:15 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,5 +4,15 @@
 import PackageDescription
 
 let package = Package(
-  name: "Swiftline"
+  name: "Swiftline",
+  products: [
+    .library(
+      name: "Swiftline",
+      targets: ["Swiftline"],
+  ],
+  targets: [
+    .target(
+      name: "Swiftline",
+      dependencies: []),
+  ],
 )
