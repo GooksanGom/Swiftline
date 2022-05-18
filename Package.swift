a5ddb0d48b29572e079807831bd5df76683a80ac
GooksanGom <stolen6394@gmail.com>
2022-05-18 18:59:52 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
   ],
   dependencies: [],
   targets: [
-    .target(name: "Swiftline", dependencies: [], path: "Sources"),
+    .target(name: "Swiftline", dependencies: [], path: "Source"),
   ]
 )
